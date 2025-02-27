591f73363d7ec59400bd01ab411ab2b1535f42ad
Alessandro Fiorillo <alessandro.fiorillo@mail.polimi.it>
2025-02-27 22:57:46 +0100

    Refactoring

--- ddr_overlays/__init__.py
+++ ddr_overlays/__init__.py
@@ -42,10 +42,34 @@ def initialize(rhapi):
     def ddr_overlays_streamBar():
         return templating.render_template('stream/bar.html', serverInfo=None, getOption=rhapi.db.option, __=rhapi.__, DEBUG=False)
 
-    @bp.route('/ddr_overlays/stream/next_up')
-    def ddr_overlays_streamNextUp():
+    @bp.route('/ddr_overlays/stream/next_up/<string:bracket_type>/<int:class_id>')
+    def ddr_overlays_streamNextUp(bracket_type, class_id):
         return templating.render_template('stream/next_up.html', serverInfo=None, getOption=rhapi.db.option, __=rhapi.__, DEBUG=False,
-            num_nodes=8
+            bracket_type=bracket_type, class_id=class_id, num_nodes=8
+        )
+
+    @bp.route('/ddr_overlays/stream/leaderboard/<string:bracket_type>/<int:class_id>')
+    def ddr_overlays_streamLeaderboard(bracket_type, class_id):
+        return templating.render_template('stream/leaderboard.html', serverInfo=None, getOption=rhapi.db.option, __=rhapi.__, DEBUG=False,
+            bracket_type=bracket_type, class_id=class_id
+        )
+
+    @bp.route('/ddr_overlays/stream/leaderboard_pages/<string:bracket_type>/<int:class_id>')
+    def ddr_overlays_streamLeaderboardPages(bracket_type, class_id):
+        return templating.render_template('stream/leaderboard_pages.html', serverInfo=None, getOption=rhapi.db.option, __=rhapi.__, DEBUG=False,
+            bracket_type=bracket_type, class_id=class_id
+        )
+
+    @bp.route('/ddr_overlays/stream/brackets/<string:bracket_type>/<int:class_id>')
+    def ddr_overlays_streamBrackets(bracket_type, class_id):
+        return templating.render_template('stream/brackets.html', serverInfo=None, getOption=rhapi.db.option,__=rhapi.__,
+            bracket_type=bracket_type, class_id=class_id
+        )
+
+    @bp.route('/ddr_overlays/stream/last_heat/<string:bracket_type>/<int:class_id>')
+    def ddr_overlays_streamLastHeat(bracket_type, class_id):
+        return templating.render_template('stream/last_heat.html', serverInfo=None, getOption=rhapi.db.option, __=rhapi.__, DEBUG=False,
+            bracket_type=bracket_type, class_id=class_id
         )
 
     @bp.route('/ddr_overlays/stream/node/<int:node_id>')
@@ -57,32 +81,7 @@ def initialize(rhapi):
         else:
             return False
 
-    @bp.route('/ddr_overlays/stream/leaderboard/<int:class_id>')
-    def ddr_overlays_streamLeaderboard32(class_id):
-        return templating.render_template('stream/leaderboard_32.html', serverInfo=None, getOption=rhapi.db.option, __=rhapi.__, DEBUG=False,
-            class_id=class_id
-        )
-
-    @bp.route('/ddr_overlays/stream/leaderboard_pages/<int:class_id>')
-    def ddr_overlays_streamLeaderboard32Pages(class_id):
-        return templating.render_template('stream/leaderboard_32_pages.html', serverInfo=None, getOption=rhapi.db.option, __=rhapi.__, DEBUG=False,
-            class_id=class_id
-        )
-
-    @bp.route('/ddr_overlays/stream/brackets/<string:bracket_type>/<int:numb_pilots>/<string:class_title>')
-    def ddr_overlays_streamBrackets(bracket_type, numb_pilots, class_title):
-        return templating.render_template('stream/brackets.html', serverInfo=None, getOption=rhapi.db.option,__=rhapi.__,
-            bracket_type=bracket_type, numb_pilots=numb_pilots, class_title=class_title
-        )
-
-    @bp.route('/ddr_overlays/stream/last_heat/<int:numb_pilots>/<string:class_title>')
-    def ddr_overlays_streamLastHeat(numb_pilots, class_title):
-        return templating.render_template('stream/last_heat.html', serverInfo=None, getOption=rhapi.db.option, __=rhapi.__, DEBUG=False,
-            numb_pilots=numb_pilots, class_title=class_title
-        )
-
     rhapi.ui.blueprint_add(bp)
 
     rhapi.ui.register_panel("ddr_overlays", "DDR - OBS Overlays", "settings")
     rhapi.ui.register_markdown("ddr_overlays", "DDR Overlays link", "Overlays are available [here](/ddr_overlays)")
-
